f1c03881538afa008f9fd212b29edaf7dbfda51d
zendzo <zendzzo@gmail.com>
2018-08-20 11:45:51 +0700

    has many through relationship, get users posts by country from country model

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,4 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -62,4 +50,11 @@ Route::get('/post/tags', function () {
 
     $post->tags()->toggle($tag);
 
+});
+
+//has many through relationship
+// get posts from users by user's country id  $country->posts
+Route::get('/country/post/{id}',function($id){
+	$country = App\Country::findOrFail($id);
+	return $country->posts;
 });
